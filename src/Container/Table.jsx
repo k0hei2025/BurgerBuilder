3412012d1affa3ccab42039f2aebd05d98921afd
バイバウ <60768219+k0hei2025@users.noreply.github.com>
2021-10-01 20:33:50 +0530

    hacktober-fest

--- src/Container/Table.jsx
+++ src/Container/Table.jsx
@@ -1,7 +1,19 @@
 import React, { Fragment } from 'react'
 import TableContainer from '../Component/table'
+import {useState} from 'react'
 
 export default function Table() {
+  
+  
+  const [add  , setAdd] =  useState("");
+  
+  func()=>{
+   setAdd("dfdd")
+  }
+  func();
+  
+  console.log("add" ,add);
+  
                return (
  
                               <Fragment>
